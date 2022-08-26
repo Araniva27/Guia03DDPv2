490cf7ea563eb37dd38ffe9af7f8ce9cc2a7a86d
Araniva27 <manuelaraniva07@gmail.com>
2022-08-25 21:51:12 -0600

    primera version

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, Image } from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry} from 'react-native'
+
+
 
 export default function App() {
+  const renderItem = ({ item }) => (
+    <Item title={item.title} img={item.src}/>
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+      data={DATA}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}/>
+    </SafeAreaView>
+   
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+const DATA = [
+  {
+    id: '1',
+    title: 'Toyota',
+    src:require('./src/imgs/toyota.jpeg')
   },
+  {
+    id: '2',
+    title: 'Mazda',
+    src:require('./src/imgs/mazda.jpg')
+  },
+  {
+    id: '3',
+    title: 'Mitsubishi',
+    src:require('./src/imgs/mitsubishi.jpg')
+  }
+];
+
+const Item = ({title, img}) =>(
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+    <Image styles={styles.img} source={img} />
+  </View>
+);
+
+//AppRegistry.registerComponent("guia03DDP", ()=>App)
+
+/** <View style={styles.container}>
+      <Text>Hola</Text>
+      <StatusBar style="auto" />
+    </View> */
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+      backgroundColor: '#f9c2ff',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+      alignItems: 'center'
+    },
+    title: {
+      fontSize: 32,
+    },
+    img:{
+      width: 200,
+      height: 125,
+      borderWidth: 2,
+      borderColor: '#d35647',
+      resizeMode: 'contain',
+      margin: 8
+    }
 });
